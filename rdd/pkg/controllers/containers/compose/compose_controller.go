mook-as/rd2

RDD: containers: implement compose reconcilers
This implements the meat of the reconcilers, for both `Compose` and
`ComposeUpRequest` objects.

Signed-off-by: Mark Yen <mark.yen@suse.com>

rdd/pkg/controllers/containers/compose/compose_controller.go
@@ -9,6 +9,7 @@ import (
 	_ "embed"
 
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/apis/containers/v1alpha1"
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/controllers/base"
@@ -73,13 +74,19 @@ func (c *controller) GetWebhookManagers() []base.WebhookManager {
 func (c *controller) setupReconciler(ctx context.Context, mgr ctrl.Manager) error {
 	mgr.GetLogger().Info("Setting up compose reconciler")
 	if err := (&reconciler{
-		Client: mgr.GetClient(),
+		ctx:          ctx,
+		Client:       mgr.GetClient(),
+		procs:        newProcessTracker(),
+		completionCh: make(chan event.TypedGenericEvent[*v1alpha1.Compose], 1024),
 	}).SetupWithManager(ctx, mgr); err != nil {
 		return err
 	}
 
 	return (&upRequestReconciler{
-		Client: mgr.GetClient(),
+		ctx:          ctx,
+		Client:       mgr.GetClient(),
+		procs:        newProcessTracker(),
+		completionCh: make(chan event.TypedGenericEvent[*v1alpha1.ComposeUpRequest], 1024),
 	}).SetupWithManager(mgr)
 }
 

rdd/pkg/controllers/containers/compose/compose_reconciler.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"slices"
 	"strings"
+	"time"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	apimeta "k8s.io/apimachinery/pkg/api/meta"
@@ -25,6 +26,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/apis/containers/v1alpha1"
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/controllers/base"
@@ -50,6 +52,12 @@ type composeRequest struct {
 // reconciler implements the Compose reconcile loop.
 type reconciler struct {
 	client.Client
+	// ctx is the context that lasts for the lifetime of the reconciler; used
+	// for the `docker compose down` process itself, which must outlive any
+	// individual reconcile.
+	ctx          context.Context
+	procs        *processTracker
+	completionCh chan event.TypedGenericEvent[*v1alpha1.Compose]
 }
 
 // +kubebuilder:rbac:groups=containers.rancherdesktop.io,resources=composes,verbs=get;list;watch;create;update;patch;delete
@@ -62,7 +70,7 @@ type reconciler struct {
 func (r *reconciler) Reconcile(ctx context.Context, req composeRequest) (ctrl.Result, error) {
 	switch req.Kind {
 	case v1alpha1.ComposeKind:
-		return ctrl.Result{}, nil
+		return r.reconcileCompose(ctx, req)
 	case v1alpha1.ContainerKind, v1alpha1.ImageKind, v1alpha1.VolumeKind:
 		return r.reconcileFromResource(ctx, req)
 	default:
@@ -75,6 +83,155 @@ func (r *reconciler) Reconcile(ctx context.Context, req composeRequest) (ctrl.Re
 		)
 		return ctrl.Result{}, nil
 	}
+}
+
+// reconcileCompose handles a reconcile request for a Compose resource: it
+// adds a finalizer to non-deleted Compose objects (so their deletion can be
+// intercepted to run `docker compose down`), runs `docker compose down` once
+// deletion has been requested, and removes the finalizer once that command
+// has completed and the project has no members left.
+func (r *reconciler) reconcileCompose(ctx context.Context, req composeRequest) (ctrl.Result, error) {
+	var project v1alpha1.Compose
+	if err := r.Get(ctx, req.NamespacedName, &project); err != nil {
+		if apierrors.IsNotFound(err) {
+			// The project no longer exists; remove any tracked process state.
+			if err := r.procs.abort(ctx, req.UID); err != nil {
+				logf.FromContext(ctx).V(1).Info(
+					"failed to kill process for deleted compose project", "name", req.NamespacedName, "err", err)
+			}
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if project.GetDeletionTimestamp().IsZero() {
+		// Not being deleted; make sure the finalizer is present so that we can
+		// intercept deletion to run `docker compose down`.
+		if changed, err := base.EnsureCleanupFinalizer(ctx, r.Client, &project); err != nil {
+			return ctrl.Result{}, fmt.Errorf(
+				"failed to add finalizer to compose project %s: %w", req.NamespacedName, err)
+		} else if changed {
+			// The finalizer was added; requeue to continue processing the project.
+			return ctrl.Result{}, nil
+		}
+		hasMembers := apimeta.FindStatusCondition(project.Status.Conditions, v1alpha1.ComposeConditionHasMembers)
+		if hasMembers == nil {
+			// Since we only create Compose objects from reconciles triggered by
+			// resources, getting here means we're in the middle of that.  Don't do
+			// anything, let it finish.
+			return ctrl.Result{}, nil
+		} else if hasMembers.Status == metav1.ConditionUnknown {
+			// HasMembers needs to be reconciled.
+			return ctrl.Result{}, retry.RetryOnConflict(retry.DefaultRetry, func() error {
+				var latest v1alpha1.Compose
+				if err := r.Get(ctx, req.NamespacedName, &latest); err != nil {
+					return err
+				}
+				status := metav1.ConditionFalse
+				reason := v1alpha1.ComposeHasMembersReasonDeleted
+				message := "no members found"
+				if len(latest.Status.Members) > 0 {
+					status = metav1.ConditionTrue
+					reason = v1alpha1.ComposeHasMembersReasonFound
+					message = fmt.Sprintf("found %d members", len(latest.Status.Members))
+				}
+				apimeta.SetStatusCondition(&latest.Status.Conditions, metav1.Condition{
+					Type:    v1alpha1.ComposeConditionHasMembers,
+					Status:  status,
+					Reason:  reason,
+					Message: message,
+				})
+				return r.Status().Update(ctx, &latest)
+			})
+		} else if hasMembers.Status == metav1.ConditionFalse {
+			// No members; queue for deletion.
+			reapDelay := getReapDelay(&project)
+			if hasMembers.LastTransitionTime.Add(reapDelay).Before(time.Now()) {
+				//nolint:gocritic // uncheckedInlineErr doesn't understand IgnoreNotFound
+				if err := r.Delete(ctx, &project); client.IgnoreNotFound(err) != nil {
+					return ctrl.Result{}, fmt.Errorf(
+						"failed to delete compose project %s: %w", req.NamespacedName, err)
+				}
+				return ctrl.Result{RequeueAfter: 1}, nil
+			}
+			return ctrl.Result{RequeueAfter: reapDelay / 2}, nil
+		}
+		return ctrl.Result{}, nil
+	}
+
+	if !base.HasCleanupFinalizer(&project) {
+		// Being deleted, and the finalizer is already gone; nothing more to do.
+		return ctrl.Result{}, nil
+	}
+
+	state, hasState := r.procs.get(project.GetUID())
+	switch {
+	case hasState && !state.finished:
+		// `docker compose down` is still running; wait for it to complete.
+		return ctrl.Result{}, nil
+	case hasState && len(project.Status.Members) > 0:
+		// `docker compose down` has finished, but there are still members; wait for
+		// them to be removed, at which point that will cause a reconcile.
+		return ctrl.Result{}, nil
+	case hasState:
+		// `docker compose down` has finished; remove the tracked state and the
+		// finalizer, letting the actual deletion proceed.
+		if state.err != nil {
+			logf.FromContext(ctx).Error(state.err,
+				"docker compose down failed; removing finalizer anyway",
+				"name", req.NamespacedName, "output", state.cmd.output())
+		}
+		err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			var latest v1alpha1.Compose
+			if err := r.Get(ctx, req.NamespacedName, &latest); err != nil {
+				return client.IgnoreNotFound(err)
+			}
+			return base.RemoveCleanupFinalizer(ctx, r.Client, &latest)
+		})
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf(
+				"failed to remove finalizer from compose project %s: %w", req.NamespacedName, err)
+		}
+		// Only remove the state after we've succeeded in removing the finalizer, so
+		// that if we exited early we can do a retry later.
+		r.procs.delete(project.GetUID())
+		return ctrl.Result{}, nil
+	default:
+		// Not yet started; kick off `docker compose down`, best-effort (only
+		// status.workingDir/status.configs may be available; if unset, compose
+		// will still be able to act using the recorded labels via
+		// --project-name, but may not find every resource).
+		if err := r.initiateProjectDown(&project); err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
+}
+
+// initiateProjectDown starts `docker compose down` for project.
+func (r *reconciler) initiateProjectDown(project *v1alpha1.Compose) error {
+	_, err := r.procs.run(
+		r.ctx,
+		project.GetUID(),
+		project.GetResourceVersion(),
+		project.Status.WorkingDir,
+		project.Status.Name,
+		project.Status.Configs,
+		[]string{"down", "--remove-orphans", "--volumes"},
+		func() {
+			completionEvent := event.TypedGenericEvent[*v1alpha1.Compose]{
+				Object: project,
+			}
+			select {
+			case r.completionCh <- completionEvent:
+			case <-r.ctx.Done():
+			}
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to run docker compose down for %s: %w", client.ObjectKeyFromObject(project), err)
+	}
+	return nil
 }
 
 // reconcileFromResource creates or updates a Compose object, as identified by
@@ -118,16 +275,17 @@ func (r *reconciler) reconcileFromResource(ctx context.Context, req composeReque
 		}
 
 		// Create the Compose project if it doesn't exist yet.
-		//nolint:gocritic // Linter doesn't understand IgnoreAlreadyExists
-		if err := r.Create(ctx, project); client.IgnoreAlreadyExists(err) != nil {
+		err = r.Create(ctx, project)
+		if apierrors.IsAlreadyExists(err) {
+			if err := r.Get(ctx, client.ObjectKeyFromObject(project), project); err != nil {
+				return err
+			}
+		} else if err != nil {
 			return fmt.Errorf(
 				"failed to create compose project for %s %s: %w", req.Kind, obj.GetName(), err)
 		}
 
 		currentMember := fmt.Sprintf("%s/%s", req.Kind, obj.GetName())
-		if err := r.Get(ctx, client.ObjectKeyFromObject(project), project); err != nil {
-			return err
-		}
 		project.Status.Namespace = namespace
 		project.Status.Name = projectName
 		if labels[composeWorkingDirLabel] != "" {
@@ -286,6 +444,14 @@ func (r *reconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) err
 
 	return builder.TypedControllerManagedBy[composeRequest](mgr).
 		Named("compose-reconciler").
+		WatchesRawSource(source.TypedChannel(r.completionCh,
+			handler.TypedEnqueueRequestsFromMapFunc(func(_ context.Context, project *v1alpha1.Compose) []composeRequest {
+				return []composeRequest{{
+					Kind:           v1alpha1.ComposeKind,
+					NamespacedName: client.ObjectKeyFromObject(project),
+					UID:            project.GetUID(),
+				}}
+			}))).
 		Watches(&v1alpha1.Container{}, enqueueRequestsForKind(v1alpha1.ContainerKind), hasLabelPredicates).
 		Watches(&v1alpha1.Image{}, enqueueRequestsForKind(v1alpha1.ImageKind), hasLabelPredicates).
 		Watches(&v1alpha1.Volume{}, enqueueRequestsForKind(v1alpha1.VolumeKind), hasLabelPredicates).

rdd/pkg/controllers/containers/compose/compose_reconciler_test.go
@@ -21,6 +21,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/apis/containers/v1alpha1"
 )
@@ -92,6 +93,19 @@ func membersIndexFunc(obj client.Object) []string {
 }
 
 // newReconciler builds a reconciler backed by a fake client seeded with objs.
+// fakeCommandExecutor returns a commandExecutor stub for tests that don't
+// exercise the `docker compose up`/`down` process-spawning paths, so that
+// reconciler fields can be non-nil without accidentally invoking real
+// processes.
+func fakeCommandExecutor(t *testing.T) commandExecutor {
+	t.Helper()
+	return func(_ context.Context, _, _ string, _ ...string) (command, error) {
+		//nolint:forbidigo // t.Fatal because this should never be reached.
+		t.Fatal("unexpected attempt to run a command in this test")
+		return nil, nil
+	}
+}
+
 func newReconciler(t *testing.T, objs ...client.Object) (*reconciler, *spyingClient) {
 	t.Helper()
 
@@ -106,7 +120,12 @@ func newReconciler(t *testing.T, objs ...client.Object) (*reconciler, *spyingCli
 
 	fakeClient := &spyingClient{Client: builder.Build()}
 
-	return &reconciler{Client: fakeClient}, fakeClient
+	return &reconciler{
+		ctx:          context.Background(),
+		Client:       fakeClient,
+		procs:        &processTracker{executor: fakeCommandExecutor(t)},
+		completionCh: make(chan event.TypedGenericEvent[*v1alpha1.Compose], 16),
+	}, fakeClient
 }
 
 // newContainer builds a Container mirror with the given k8s namespace/name,

mook-as/rancher-desktop-2

rdd/pkg/controllers/containers/compose/compose_controller.go
@@ -9,6 +9,7 @@ import (
 	_ "embed"
 
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/apis/containers/v1alpha1"
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/controllers/base"
@@ -73,13 +74,19 @@ func (c *controller) GetWebhookManagers() []base.WebhookManager {
 func (c *controller) setupReconciler(ctx context.Context, mgr ctrl.Manager) error {
 	mgr.GetLogger().Info("Setting up compose reconciler")
 	if err := (&reconciler{
-		Client: mgr.GetClient(),
+		ctx:          ctx,
+		Client:       mgr.GetClient(),
+		procs:        newProcessTracker(),
+		completionCh: make(chan event.TypedGenericEvent[*v1alpha1.ComposeProject], 1024),
 	}).SetupWithManager(ctx, mgr); err != nil {
 		return err
 	}
 
 	return (&upRequestReconciler{
-		Client: mgr.GetClient(),
+		ctx:          ctx,
+		Client:       mgr.GetClient(),
+		procs:        newProcessTracker(),
+		completionCh: make(chan event.TypedGenericEvent[*v1alpha1.ComposeUpRequest], 1024),
 	}).SetupWithManager(mgr)
 }
 

rdd/pkg/controllers/containers/compose/compose_reconciler.go
@@ -11,6 +11,7 @@ import (
 	"path/filepath"
 	"slices"
 	"strings"
+	"time"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	apimeta "k8s.io/apimachinery/pkg/api/meta"
@@ -20,10 +21,12 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/apis/containers/v1alpha1"
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/controllers/base"
@@ -53,6 +56,12 @@ type composeRequest struct {
 // reconciler implements the ComposeProject reconcile loop.
 type reconciler struct {
 	client.Client
+	// ctx is the context that lasts for the lifetime of the reconciler; used
+	// for the `docker compose down` process itself, which must outlive any
+	// individual reconcile.
+	ctx          context.Context
+	procs        *processTracker
+	completionCh chan event.TypedGenericEvent[*v1alpha1.ComposeProject]
 }
 
 // +kubebuilder:rbac:groups=containers.rancherdesktop.io,resources=composeprojects,verbs=get;list;watch;create;update;patch;delete
@@ -65,7 +74,7 @@ type reconciler struct {
 func (r *reconciler) Reconcile(ctx context.Context, req composeRequest) (ctrl.Result, error) {
 	switch req.Kind {
 	case v1alpha1.ComposeProjectKind:
-		return ctrl.Result{}, nil
+		return r.reconcileCompose(ctx, req)
 	case v1alpha1.ContainerKind:
 		return r.reconcileContainer(ctx, req)
 	default:
@@ -78,6 +87,162 @@ func (r *reconciler) Reconcile(ctx context.Context, req composeRequest) (ctrl.Re
 		)
 		return ctrl.Result{}, nil
 	}
+}
+
+// reconcileCompose handles a reconcile request for a Compose resource: it
+// adds a finalizer to non-deleted Compose objects (so their deletion can be
+// intercepted to run `docker compose down`), runs `docker compose down` once
+// deletion has been requested, and removes the finalizer once that command
+// has completed and the project has no members left.
+func (r *reconciler) reconcileCompose(ctx context.Context, req composeRequest) (ctrl.Result, error) {
+	var project v1alpha1.ComposeProject
+	if err := r.Get(ctx, req.NamespacedName, &project); err != nil {
+		if apierrors.IsNotFound(err) {
+			// The project no longer exists; remove any tracked process state.
+			if err := r.procs.abort(ctx, req.UID); err != nil {
+				logf.FromContext(ctx).V(1).Info(
+					"failed to kill process for deleted compose project", "name", req.NamespacedName, "err", err)
+			}
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	if project.GetDeletionTimestamp().IsZero() {
+		// Not being deleted; make sure the finalizer is present so that we can
+		// intercept deletion to run `docker compose down`.
+		if controllerutil.AddFinalizer(&project, mirrorFinalizer) {
+			if err := r.Update(ctx, &project); err != nil {
+				return ctrl.Result{}, fmt.Errorf(
+					"failed to add finalizer to compose project %s: %w", req.NamespacedName, err)
+			}
+			// The finalizer was added; requeue to continue processing the project.
+			return ctrl.Result{}, nil
+		}
+		hasMembers := apimeta.FindStatusCondition(project.Status.Conditions, v1alpha1.ComposeProjectConditionHasMembers)
+		if hasMembers == nil {
+			// Since we only create Compose objects from reconciles triggered by
+			// resources, getting here means we're in the middle of that.  Don't do
+			// anything, let it finish.
+			return ctrl.Result{}, nil
+		} else if hasMembers.Status == metav1.ConditionUnknown {
+			// HasMembers needs to be reconciled.
+			return ctrl.Result{}, retry.RetryOnConflict(retry.DefaultRetry, func() error {
+				var latest v1alpha1.ComposeProject
+				if err := r.Get(ctx, req.NamespacedName, &latest); err != nil {
+					return err
+				}
+				status := metav1.ConditionFalse
+				reason := v1alpha1.ComposeHasMembersReasonDeleted
+				message := "no members found"
+				if len(latest.Status.Containers) > 0 {
+					status = metav1.ConditionTrue
+					reason = v1alpha1.ComposeHasMembersReasonFound
+					message = fmt.Sprintf("found %d members", len(latest.Status.Containers))
+				}
+				apimeta.SetStatusCondition(&latest.Status.Conditions, metav1.Condition{
+					Type:    v1alpha1.ComposeProjectConditionHasMembers,
+					Status:  status,
+					Reason:  reason,
+					Message: message,
+				})
+				return r.Status().Update(ctx, &latest)
+			})
+		} else if hasMembers.Status == metav1.ConditionFalse {
+			// No members; queue for deletion.
+			reapDelay := getReapDelay(&project)
+			targetTime := hasMembers.LastTransitionTime.Add(reapDelay)
+			if targetTime.Before(time.Now()) {
+				//nolint:gocritic // uncheckedInlineErr doesn't understand IgnoreNotFound
+				if err := r.Delete(ctx, &project); client.IgnoreNotFound(err) != nil {
+					return ctrl.Result{}, fmt.Errorf(
+						"failed to delete compose project %s: %w", req.NamespacedName, err)
+				}
+				return ctrl.Result{RequeueAfter: time.Second}, nil
+			}
+			return ctrl.Result{RequeueAfter: time.Until(targetTime)}, nil
+		}
+		return ctrl.Result{}, nil
+	}
+
+	if !controllerutil.ContainsFinalizer(&project, mirrorFinalizer) {
+		// Being deleted, and the finalizer is already gone; nothing more to do.
+		return ctrl.Result{}, nil
+	}
+
+	state, hasState := r.procs.get(project.GetUID())
+	switch {
+	case hasState && !state.finished:
+		// `docker compose down` is still running; wait for it to complete.
+		return ctrl.Result{}, nil
+	case hasState && len(project.Status.Containers) > 0:
+		// `docker compose down` has finished, but there are still members; wait for
+		// them to be removed, at which point that will cause a reconcile.
+		return ctrl.Result{}, nil
+	case hasState:
+		// `docker compose down` has finished; remove the tracked state, and return
+		// the error so we can retry later.
+		if state.err != nil {
+			logf.FromContext(ctx).Error(state.err,
+				"docker compose down failed; will retry",
+				"name", req.NamespacedName, "output", state.cmd.output())
+			r.procs.delete(project.GetUID())
+			return ctrl.Result{}, state.err
+		}
+		err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			var latest v1alpha1.ComposeProject
+			if err := r.Get(ctx, req.NamespacedName, &latest); err != nil {
+				return client.IgnoreNotFound(err)
+			}
+			if controllerutil.RemoveFinalizer(&latest, mirrorFinalizer) {
+				return r.Update(ctx, &latest)
+			}
+			return nil
+		})
+		if err != nil {
+			return ctrl.Result{}, fmt.Errorf(
+				"failed to remove finalizer from compose project %s: %w", req.NamespacedName, err)
+		}
+		// Only remove the state after we've succeeded in removing the finalizer, so
+		// that if we exited early we can do a retry later.
+		r.procs.delete(project.GetUID())
+		return ctrl.Result{}, nil
+	default:
+		// Not yet started; kick off `docker compose down`, best-effort (only
+		// status.workingDir/status.configs may be available; if unset, compose
+		// will still be able to act using the recorded labels via
+		// --project-name, but may not find every resource).
+		if err := r.initiateProjectDown(&project); err != nil {
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
+}
+
+// initiateProjectDown starts `docker compose down` for project.
+func (r *reconciler) initiateProjectDown(project *v1alpha1.ComposeProject) error {
+	err := r.procs.run(
+		r.ctx,
+		project.GetUID(),
+		project.GetResourceVersion(),
+		project.Status.WorkingDir,
+		project.Status.Name,
+		project.Status.Configs,
+		[]string{"down", "--remove-orphans", "--volumes", "--rmi", "all"},
+		func() {
+			completionEvent := event.TypedGenericEvent[*v1alpha1.ComposeProject]{
+				Object: project,
+			}
+			select {
+			case r.completionCh <- completionEvent:
+			case <-r.ctx.Done():
+			}
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to run docker compose down for %s: %w", client.ObjectKeyFromObject(project), err)
+	}
+	return nil
 }
 
 // reconcileContainer creates or updates a ComposeProject object, based on the
@@ -273,5 +438,13 @@ func (r *reconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) err
 				UID:            obj.GetUID(),
 			}}
 		})).
+		WatchesRawSource(source.TypedChannel(r.completionCh,
+			handler.TypedEnqueueRequestsFromMapFunc(func(_ context.Context, project *v1alpha1.ComposeProject) []composeRequest {
+				return []composeRequest{{
+					Kind:           v1alpha1.ComposeProjectKind,
+					NamespacedName: client.ObjectKeyFromObject(project),
+					UID:            project.GetUID(),
+				}}
+			}))).
 		Complete(r)
 }

rdd/pkg/controllers/containers/compose/compose_reconciler_test.go
@@ -19,6 +19,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 
 	"github.com/rancher-sandbox/rancher-desktop-daemon/pkg/apis/containers/v1alpha1"
 )
@@ -90,6 +91,19 @@ func membersIndexFunc(obj client.Object) []string {
 }
 
 // newReconciler builds a reconciler backed by a fake client seeded with objs.
+// fakeCommandExecutor returns a commandExecutor stub for tests that don't
+// exercise the `docker compose up`/`down` process-spawning paths, so that
+// reconciler fields can be non-nil without accidentally invoking real
+// processes.
+func fakeCommandExecutor(t *testing.T) commandExecutor {
+	t.Helper()
+	return func(_ context.Context, _, _ string, _ ...string) (command, error) {
+		//nolint:forbidigo // t.Fatal because this should never be reached.
+		t.Fatal("unexpected attempt to run a command in this test")
+		return nil, nil
+	}
+}
+
 func newReconciler(t *testing.T, objs ...client.Object) (*reconciler, *spyingClient) {
 	t.Helper()
 
@@ -104,7 +118,12 @@ func newReconciler(t *testing.T, objs ...client.Object) (*reconciler, *spyingCli
 
 	fakeClient := &spyingClient{Client: builder.Build()}
 
-	return &reconciler{Client: fakeClient}, fakeClient
+	return &reconciler{
+		ctx:          context.Background(),
+		Client:       fakeClient,
+		procs:        &processTracker{executor: fakeCommandExecutor(t)},
+		completionCh: make(chan event.TypedGenericEvent[*v1alpha1.ComposeProject], 16),
+	}, fakeClient
 }
 
 // newContainer builds a Container mirror with the given k8s namespace/name,
@@ -371,7 +390,7 @@ func TestReconcile_dispatch(t *testing.T) {
 		assert.DeepEqual(t, result, ctrl.Result{})
 	})
 
-	t.Run("no-ops for ComposeKind", func(t *testing.T) {
+	t.Run("no-ops for ComposeProjectKind", func(t *testing.T) {
 		t.Parallel()
 		r, fakeClient := newReconciler(t)
 		result, err := r.Reconcile(t.Context(), composeRequest{